0256644933e873a6c6365a05f5fa8abcc422137b
Zainan <zainanzaher09@gmail.com>
2024-10-25 02:14:02 +0100

    Match-Today Component

--- components/matchtoday.jsx
+++ components/matchtoday.jsx
@@ -2,13 +2,45 @@ import { View, Text, StyleSheet } from "react-native";
 export default function MatchToday() {
   return (
     <View style={styles.container}>
-      <Text>This Compoentn</Text>
+      <View style={styles.avatarsContainer}>
+        <View style={styles.avatar} />
+        <View style={styles.avatar} />
+        <View style={styles.avatar} />
+      </View>
+      <Text style={styles.descriptionText}>Match of the Day</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  descriptionText: {
+    fontSize: 20,
+    width: "60%",
+    textAlign: "center",
+  },
+  avatarsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "40%",
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    backgroundColor: "gray",
+  },
   container: {
-    width: "20%",
+    width: "85%",
+    height: "10%",
+    borderRadius: 17,
+    backgroundColor: "white",
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "center",
+    marginVertical: 25,
+    justifyContent: "space-evenly",
+    paddingHorizontal: 10,
+    borderColor: "#999",
+    borderWidth: 1,
   },
 });
